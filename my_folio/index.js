6e7bb5cd1a90b7dbee790ce2d21669c8fa9de8e3
Rlndinesh <rlndinesh@gmail.com>
2025-12-28 00:19:10 +0530

    half completed

---
+++ my_folio/index.js
@@ -0,0 +1,9 @@
+var icon=document.getElementById("icon");
+icon.onclick=function(){
+    document.body.classList.toggle("light-theme")
+    if (document.body.classList.contains("light-theme")){
+        icon.textContent='⏾';
+    }else{
+        icon.textContent='☀︎';
+    }
+}
